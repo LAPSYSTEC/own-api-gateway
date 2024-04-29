97d170cfdbbdf44b515b3fece3efa42c78af2c74
Luigi Vismara <33238929+luigivis@users.noreply.github.com>
2024-04-28 18:40:18 -0600

    feat: add basic structure

---
+++ src-own-api-gateway/src/main/java/com/luigivis/srcownapigateway/SrcOwnApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.luigivis.srcownapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@SpringBootApplication
+public class SrcOwnApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrcOwnApiGatewayApplication.class, args);
+    }
+
+}
